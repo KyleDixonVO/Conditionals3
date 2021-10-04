b49de7c947eea4722c90aa7d92f7c12f64adaa5d
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-04 12:52:31 -0300

    Update Program.cs

--- Conditonals3/Program.cs
+++ Conditonals3/Program.cs
@@ -8,8 +8,25 @@ namespace Conditonals3
 {
     class Program
     {
+        static bool alive = true;
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals 3");
+
+            if (alive == true)
+            {
+                Console.WriteLine("You are alive! :)");
+            }
+            else
+            {
+                Console.WriteLine("You are dead. :(");
+            }
+
+
+
+
+            Console.ReadKey(true);
         }
+
     }
 }
